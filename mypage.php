e7493fbbc04cedc39f49608aaaabc949fa6b7d1b
dldmz1 <wwd0203@daum.net>
2022-12-07 01:05:59 +0900

    [ADD] mypage css 추가

--- mypage.php
+++ mypage.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>MY PAGE</title>
+        <link rel="stylesheet" href="css/signup.css">
         <?php
             session_start();
 
@@ -18,35 +19,125 @@
             $result = mysqli_query($conn, $sql);
             $res = mysqli_fetch_array($result);
         ?>
+        <script>
+            function check_pw() {
+                var id = document.getElementById("id").innerHTML;
+                var url = "check_pw.php?id=" + id;
+                window.open(url, "chkpw", 'width=500,height=800, scrollbars=no, resizable=no');
+            }
+            function change_pw() {
+                document.getElementById("pw").disabled = false;
+                document.getElementById("pw_button").value = "확정";
+                document.getElementById("pw_button").style.color = "hotpink";
+                document.getElementById("pw_button").setAttribute("onclick", "decide_pw()");
+            }
+            function decide_pw() {
+                document.getElementById("submit").disabled = false;
+                document.getElementById("pw2").value = document.getElementById("pw").value;
+                document.getElementById("pw").disabled = true;
+                document.getElementById("pw_button").disabled = true;
+                document.getElementById("pw_button").value = "확정됨";
+                document.getElementById("pw_button").style.color = "#ccc";
+            }
+        </script>
     </head>
     <body>
-        <div class=mypage>
-            <table>
-                <tr>
-                    <th>아이디</th>
-                    <td><?=$res['아이디']?></td>
-                </tr>
-                <tr>
-                    <th>나이</th>
-                    <td><?=$res['나이']?></td>
-                </tr>
-                <tr>
-                    <th>성별</th>
-                    <td><?=$res['성별']?></td>
-                </tr>
-                <tr>
-                    <th>MBTI</th>
-                    <td><?=$res['mbti']?></td>
-                </tr>
-                <tr>
-                    <td>
-                        <button onclick="location.href='user_edit.php'">회원정보 수정</button>
-                    </td>
-                    <td>
-                        <button onclick="location.href='user_delete.php'">회원 탈퇴</button>
-                    </td>
-                </tr>
-            </table>
+        <div id="wrap">
+            <div id="header" class="DonutJoinform">
+                <h1 id="top_logo">
+                    <span class="blind">
+                        <div class="h_text">Profile</div>
+                    </span>
+                </h1>
+            </div>
+            <form method="post" id="user_form" action="edit_user.php">
+            <div id="container">
+                <div id="content">
+                    <div class="join_content">
+                        <div class="row_group">
+
+                            <div class="join_row">
+                                <h3 class="join_title">
+                                    <label for="id">아이디</label>
+                                </h3>
+                                <span><?=$res['아이디']?></span>
+                            </div>
+
+                            <div class="join_row">
+                                <h3 class="join_title">
+                                    <label for="password">비밀번호</label>
+                                </h3>
+                                <span class="id_box int_pass">
+                                    <input type="password" name=pw id=pw disabled placeholder="필수 입력 사항입니다." value="<?=$res['비밀번호']?>">
+                                    <input type=button id=pw_button value="변경" onclick="check_pw();">
+                                </span>
+                                <input type="hidden" name=pw2 id=pw2 value="<?=$res['비밀번호']?>">
+                                
+                                <h3 class="join_title">
+                                    <label for="age">나이</label>
+                                </h3>
+                                <span class="id_box int_pass_check" id="pswd2Img">
+                                    <input type="text" name=age id=age placeholder="<?=$res['나이']?>">
+                                </span>
+                            </div>
+
+                            <div class="join_row">
+                                <h3 class="join_title">
+                                    <label for="mbti">mbti</label>
+                                </h3>
+                                <span class="id_box gender_code">
+                                    <select id="mbti" name="mbti" class="sel" aria-label="mbti">
+                                        <option value="Intj">Intj</option>
+                                        <option value="Entj">Entj</option>
+                                        <option value="Enfj">Enfj</option>
+                                        <option value="Infj">Infj</option>
+                                        <option value="Istj">Istj</option>
+                                        <option value="Estj">Estj</option>
+                                        <option value="Entp">Entp</option>
+                                        <option value="Intp">Intp</option>
+                                        <option value="Enfp">Enfp</option>
+                                        <option value="Infp">Infp</option>
+                                        <option value="Estp">Estp</option>
+                                        <option value="Istp">Istp</option>
+                                        <option value="Esfp">Esfp</option>
+                                        <option value="Isfp">Isfp</option>
+                                        <option value="Isfj">Isfj</option>
+                                        <option value="Esfj">Esfj</option>
+                                    </select>
+                                </span>
+                            </div>
+                            
+                            <div class="join_row">
+                                <h3 class="join_title">
+                                    <label for="gender">성별</label>
+                                </h3>
+                                <div class="id_box gender_code">
+                                    <select id="gender" name="gender" class="sel" aria-label="성별">
+                                        <option value="M">남자</option>
+                                        <option value="F">여자</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="btn_area">
+                    <button type="submit" id="submit" class="btn_type btn_primary">
+                        <span>EDIT</span>
+                    </button>
+                    <a href="delete_user.php" style="margin-top: 10px; display: flex; justify-content: center; align-items: center;">회원 탈퇴</a>
+                </div>
+            </div>
+            </form>
+        </div>
+        <br></br>
+
+        <div id="footer">
+            <address>
+                <em class="copy">컴퓨터융합소프트웨어학과</em>
+                <em class="u_cri">박소연, 조정인</em>
+            </address>
         </div>
     </body>
 </html>
